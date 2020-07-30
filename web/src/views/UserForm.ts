3424ec25b9701f27c9bc200d0a275ab8efd75b96
nathan-tw <106306027@nccu.edu.tw>
2020-07-30 04:55:23 -0400

    add UserForm and UserShow

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,51 +1,47 @@
-export class UserForm {
-  constructor(public parent: Element){}
+import { User, UserProps } from '../models/User';
+import { View } from './View';
 
+export class UserForm extends View<User, UserProps>{
 
-  eventsMap(): { [key: string]: () => void}{
-    return{
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick
     };
   }
-  
-  onHeaderHover(): void{
-    console.log('h1 was hovered over');
+
+  onSaveClick = (): void =>{
+    this.model.save();
   }
-  onButtonClick(): void{
-    console.log('hi there')
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    if (input) {
+      const name = input.value;
+
+      this.model.set({ name });
+    }
+
+
+  }
+
 
 
   template(): string {
     return `
         <div>
-            <h1>User Form</h1>
-            <input/>
-            <button>click me</button>
+            <input placeholder="${this.model.get('name')}"/>
+            <button class="set-name">Change Name</button>
+            <button class="set-age">Set random age</button>
+            <button class="save-model">save-model</button>
         </div>
 
         `;
   }
 
-  bindEvents(fragment: DocumentFragment): void{
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap){
-      const [eventName, selector] = eventKey.split(':');
-
-      fragment.querySelectorAll(selector).forEach(element =>{
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      })
-    }
-  }
-
-  render(): void{
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
-  }
 }
---
+++ web/src/views/View.ts
@@ -0,0 +1,64 @@
+import { Model } from '../models/Model';
+
+export abstract class View<T extends Model<K>, K>{
+  regions: { [key: string]: Element } = {};
+
+  constructor(public parent: Element, public model: T) {
+    this.bindModel();
+  }
+
+  
+  abstract template(): string;
+
+  regionsMap(): { [key: string]: string }{
+    return {}
+  }
+  eventsMap(): { [key: string]: () => void }{
+    return {}
+  };
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for ( let key in regionsMap){
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  render(): void {
+    // though this is no a efficient way like angular and react
+    // they compare the new and old DOM and re-render it
+    // we here just simply demonstrate how it work
+    this.parent.innerHTML = '';
+
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.parent.append(templateElement.content);
+  }
+}
